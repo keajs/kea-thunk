07945dfc0d1712b0e68e838d9df00e99375c309f
Marius Andra <marius.andra@gmail.com>
2017-09-28 17:10:43 +0300

    more tests

--- src/__tests__/thunk.js
+++ src/__tests__/thunk.js
@@ -83,3 +83,51 @@ test('thunks can call thunks', () => {
   expect(firstThunkRan).toBe(true)
   expect(secondThunkRan).toBe(true)
 })
+
+test('connected thunks work', () => {
+  const { store } = getStore()
+
+  let thunkRan = false
+
+  const firstLogic = kea({
+    path: () => ['scenes', 'homepage', 'first'],
+    actions: ({ constants }) => ({
+      updateName: name => ({ name })
+    }),
+    thunks: ({ actions, dispatch, getState }) => ({
+      updateNameAsync: name => {
+        thunkRan = true
+        dispatch(actions.updateName(name))
+      }
+    }),
+    reducers: ({ actions, constants }) => ({
+      name: ['chirpy', PropTypes.string, {
+        [actions.updateName]: (state, payload) => payload.name
+      }]
+    })
+  })
+
+  const secondLogic = kea({
+    connect: {
+      actions: [
+        firstLogic, [
+          'updateNameAsync'
+        ]
+      ],
+      props: [
+        firstLogic, [
+          'name'
+        ]
+      ]
+    }
+  })
+
+  expect(firstLogic._keaPlugins.thunk).toBe(true)
+  expect(Object.keys(secondLogic.actions)).toEqual(['updateNameAsync'])
+  expect(Object.keys(secondLogic.selectors).sort()).toEqual(['name'])
+
+  store.dispatch(secondLogic.actions.updateNameAsync('derpy'))
+  expect(secondLogic.selectors.name(store.getState())).toBe('derpy')
+
+  expect(thunkRan).toBe(true)
+})
