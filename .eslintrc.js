e04e306852e7073f127a7532995da45fe590bfa0
Marius Andra <marius.andra@gmail.com>
2022-04-06 00:31:47 +0300

    less lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
   },
 }
